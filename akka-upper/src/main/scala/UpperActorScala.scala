5d992c5a46d5bab7514081764d2b3608a0a64701
Vladimir Ulyanov <dedushka.lenin@gmail.com>
2015-11-24 09:08:56 +0800

    Routed Worker separated

--- akka-upper/src/main/scala/UpperActorScala.scala
+++ akka-upper/src/main/scala/UpperActorScala.scala
@@ -46,38 +46,3 @@ object UpperApp extends App {
 
 }
 
-
-class WorkerUpRouted extends Actor{
-  def receive = {
-    case message:String => {
-      println(s"$self() got message $message")
-      sender ! message.toUpperCase()
-      while(true){}
-      //Thread.sleep(5000)
-    }
-  }
-}
-object UpperPoolApp extends App {
-
-  // Create the 'demo' actor system
-  val system = ActorSystem("demo")
-
-  // Create the actors
-  val router: ActorRef = system.actorOf(RoundRobinPool(5).props(Props[WorkerUpRouted]), "router")
-  val callback = system.actorOf(Props[Callback], "callback")
-
-  val sc = new Scanner(System.in)
-
-  while (sc.hasNext) {
-    val message = sc.nextLine()
-
-    message match {
-      case "exit" => {
-        system.terminate()
-        System.exit(0)
-      }
-      case other => router.tell(other, callback)
-    }
-  }
-
-}
